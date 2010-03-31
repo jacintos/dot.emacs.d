916576152ecb971a596a6536b1cd8c82c7a1269d
jacinto <jacinto.m.shy@gmail.com>
2010-03-30 20:13:29 -0400

    Accidentally deleted the .php autoloading.

--- main-modes.el
+++ main-modes.el
@@ -1,6 +1,7 @@
 
 ;; Autoload php-mode
 (autoload 'php-mode "php-mode" "Major mode for editing php code." t)
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
 
 ;; nXhtml
